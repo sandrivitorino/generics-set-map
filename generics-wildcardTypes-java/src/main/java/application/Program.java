a8d160a219ec6ea5e9c638cdddf4c458d84f7218
vonvit <sandrineviturn@gmail.com>
2026-04-01 18:26:46 -0300

    tipos curinga

--- generics-wildcardTypes-java/src/main/java/application/Program.java
+++ generics-wildcardTypes-java/src/main/java/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
-    void main(){
-
-        List<?> myObjs = new ArrayList<Object>();
-        List<Integer> myNumbers = new ArrayList<Integer>();
-        myObjs = myNumbers;
-
+    void main() {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
+    }
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
     }
 }
