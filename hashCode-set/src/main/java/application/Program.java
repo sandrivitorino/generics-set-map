94f4e374a7599b42e415912719b95810537df600
vonvit <sandrineviturn@gmail.com>
2026-04-06 15:09:51 -0300

    HashCode - Set

--- hashCode-set/src/main/java/application/Program.java
+++ hashCode-set/src/main/java/application/Program.java
@@ -1,28 +1,21 @@
 package application;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
     void main() {
 
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+        Set<String> set = new HashSet<>();
 
-        //union
-        Set<Integer> c = new TreeSet<>(a);
-        c.addAll(b);
-        System.out.println(c);
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
 
-        //intersection
-        Set<Integer> d = new TreeSet<>(a);
-        d.retainAll(b);
-        System.out.println(d);
+        System.out.println(set.contains("Notebook"));
 
-        //difference
-        Set<Integer> e = new TreeSet<>(a);
-        e.removeAll(b);
-        System.out.println(e);
+        for (String p : set) {
+            System.out.println(p);
+        }
     }
 }
